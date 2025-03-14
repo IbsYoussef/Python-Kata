20baff345ff4da73e6bdb756977b1d3bd1469391
iyoussef <ibraheemyoussefiy@gmail.com>
2025-03-14 22:20:47 +0000

    Expanded test cases with type assertion

--- 8 kyu/Sum of positive/v1.py
+++ 8 kyu/Sum of positive/v1.py
@@ -16,10 +16,19 @@ def positive_sum(arr):
     return result
 
 # Test Cases
-test = [1, 5, -2, -6, 10]
-test2 = [1, 5, -2, -6, 10, 15, -22, 30]
-test3 = [-2, -3, -4, -5, 1]
+assert positive_sum([1, 5, -2, -6, 10]) == 16
+assert positive_sum([1, 5,-2, -6, 10, 15, -22, 30]) == 61
+assert positive_sum([-2, -3, -4, -5, 1]) == 1
 
-print(positive_sum(test)) # 16
-print(positive_sum(test2)) # 61
-print(positive_sum(test3)) # 1
+assert positive_sum([2, 4, 6, 8, 10]) == 30
+assert positive_sum([-1, -2, -3, -4, -5]) == 0
+assert positive_sum([]) == 0
+assert positive_sum([5]) == 5
+assert positive_sum([-10]) == 0
+
+assert positive_sum([1000000, -500000, 2000000, -1000000]) == 3000000
+
+assert positive_sum([0, 1, 2, 3, 0, -5]) == 6
+assert positive_sum([-1000, -2000, 500, -3000, 100]) == 600
+
+print("All tests passed!")
