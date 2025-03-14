0814841e75efa9fb63f95b8c1b80c0d7ba1f1063
iyoussef <ibraheemyoussefiy@gmail.com>
2025-03-14 22:21:08 +0000

    Sum of positive v2 solution

---
+++ 8 kyu/Sum of positive/v2.py
@@ -0,0 +1,28 @@
+# Task
+# You get an array of numbers, return the sum of all positive ones
+
+# Example
+# [1, -4, 7, 12] => 1 + 7 + 12 = 20
+# Note
+# If there is nothing to sum, the sum is default to 0.
+
+def positive_sum(arr):
+    return sum(n for n in arr if n > 0)
+
+# Test Cases
+assert positive_sum([1, 5, -2, -6, 10]) == 16
+assert positive_sum([1, 5,-2, -6, 10, 15, -22, 30]) == 61
+assert positive_sum([-2, -3, -4, -5, 1]) == 1
+
+assert positive_sum([2, 4, 6, 8, 10]) == 30
+assert positive_sum([-1, -2, -3, -4, -5]) == 0
+assert positive_sum([]) == 0
+assert positive_sum([5]) == 5
+assert positive_sum([-10]) == 0
+
+assert positive_sum([1000000, -500000, 2000000, -1000000]) == 3000000
+
+assert positive_sum([0, 1, 2, 3, 0, -5]) == 6
+assert positive_sum([-1000, -2000, 500, -3000, 100]) == 600
+
+print("All tests passed!")
